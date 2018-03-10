9dc41dc7ae629f460647fb0e73c3e19c96ec499d
Christophe VG <contact@christophe.vg>
2018-03-10 10:49:16 +0100

    adding packaging setup information

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'servicefactory',
+  packages = ['servicefactory'],
+  version = '0.2',
+  description = 'A highly opinionated and very convention-driven framework for creating Python "services"',
+  author = 'Christophe VG',
+  author_email = 'contact@christophe.vg',
+  url = 'https://github.com/christophevg/py-servicefactory',
+  download_url = 'https://github.com/christophevg/py-servicefactory/archive/0.2.tar.gz',
+  keywords = ['services', 'framework', 'rest', 'api', 'ipc'],
+  classifiers = [],
+)
